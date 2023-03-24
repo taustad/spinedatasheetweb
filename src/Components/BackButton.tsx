4badb9818864834aee68f23500c86d0d5d9dd916
Daniel Böhme <dboh@equinor.com>
2023-03-24 11:03:51 +0100

    fix: Added color when hovering over back button
    Added button component instead of just icon in back button component to have a hover color

--- src/Components/BackButton.tsx
+++ src/Components/BackButton.tsx
@@ -1,17 +1,17 @@
 import { useNavigate } from "react-router-dom"
 import { arrow_back } from "@equinor/eds-icons"
-import { Icon } from "@equinor/eds-core-react"
+import { Button, Icon } from "@equinor/eds-core-react"
 
 export function BackButton() {
     const navigate = useNavigate()
 
     return (
-        <Icon
-            data={arrow_back}
-            color="#007079"
-            onClick={() => navigate(-1)}
-            size={24}
-            title="Back to previous page"
-        />
+        <Button variant="ghost_icon" onClick={() => navigate(-1)} title="Back to previous page">
+            <Icon
+                data={arrow_back}
+                color="#007079"
+                size={24}
+            />
+        </Button>
     )
 }
